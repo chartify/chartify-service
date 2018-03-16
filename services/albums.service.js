74467f703dd5b9ff43434ff9a36661b663021477
Kirill Stepkin <kirillstyopkin@gmail.com>
2018-03-16 21:36:43 +0200

    add cors headers

--- services/albums.service.js
+++ services/albums.service.js
@@ -13,7 +13,20 @@ module.exports = {
 	 * Service settings
 	 */
 	settings: {
-
+		cors: {
+            // Configures the Access-Control-Allow-Origin CORS header.
+            origin: "*",
+            // Configures the Access-Control-Allow-Methods CORS header. 
+            methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+            // Configures the Access-Control-Allow-Headers CORS header.
+            allowedHeaders: [],
+            // Configures the Access-Control-Expose-Headers CORS header.
+            exposedHeaders: [],
+            // Configures the Access-Control-Allow-Credentials CORS header.
+            credentials: false,
+            // Configures the Access-Control-Max-Age CORS header.
+            maxAge: 3600
+        }
 	},
 
 	/**
