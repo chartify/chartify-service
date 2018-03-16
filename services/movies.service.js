93c96713c37ae9dcd01127ffee09f4511d309834
Kirill Stepkin <kirillstyopkin@gmail.com>
2018-03-16 12:43:51 +0200

    first commit

---
+++ services/movies.service.js
@@ -0,0 +1,98 @@
+"use strict";
+
+const DbService = require("moleculer-db");
+const MongoDBAdapter = require("moleculer-db-adapter-mongo");
+
+module.exports = {
+	name: "movies",
+	mixins: [DbService],
+	adapter: new MongoDBAdapter("mongodb://kirill:chartify@ds113799.mlab.com:13799/chartify"),
+	collection: "movies",
+
+	/**
+	 * Service settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Service metadata
+	 */
+	metadata: {
+
+	},
+
+	/**
+	 * Service dependencies
+	 */
+	//dependencies: [],	
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello'
+		 *
+		 * @returns
+		 */
+		hello() {
+			return "Hello Moleculer";
+		},
+
+		list() {
+			return this.actions.find();
+		},
+
+		/**
+		 * Welcome a username
+		 *
+		 * @param {String} name - User name
+		 */
+		welcome: {
+			params: {
+				name: "string"
+			},
+			handler(ctx) {
+				return `Welcome, ${ctx.params.name}`;
+			}
+		}
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+
+	}
+};
